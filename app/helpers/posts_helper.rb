b1217516bc3fac33c1d668a79e2bac67012ed98d
shilpi <gargshilpi22@gmail.com>
2012-12-24 15:06:11 -0600

    prepended post urls with http

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+
+	def display_title_url(post)
+		if post.url.starts_with?("http://")
+			return link_to post.title, post.url
+		else
+			return link_to post.title, "http://" + post.url
+		end
+	end
 end
